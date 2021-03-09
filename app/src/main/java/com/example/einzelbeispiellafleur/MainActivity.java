d384fd5854749f207133e4872d2505acb71465bd
Denies Lafleur <delafleur@edu.aau.at>
2021-03-09 10:12:29 +0100

    created OnClickListener for calculating

--- app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.einzelbeispiellafleur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calculate = findViewById(R.id.doCalculation);
+        calculate.setOnClickListener(
+                //Do Fancy Stuff
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       doCalculation();
+                    }
+                }
+        );
     }
 
 
