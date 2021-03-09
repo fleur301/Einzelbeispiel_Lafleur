c0ddb95169279b73603fbfc234d0a7bdd7dfac0b
Denies Lafleur <delafleur@edu.aau.at>
2021-03-09 11:07:53 +0100

    implemented basicfunction for task 2.1

--- app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
@@ -8,12 +8,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button connectToServer = findViewById(R.id.connectionBtn);
+        connectToServer.setOnClickListener(
+                //Do Fancy Stuff
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        connectToServer();
+                    }
+                }
+        );
+
+
+
+
 
         Button calculate = findViewById(R.id.doCalculation);
         calculate.setOnClickListener(
@@ -27,6 +49,29 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    public void connectToServer() {
+        EditText mat = findViewById(R.id.matrikelnummer);
+        try {
+
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String matnr = mat.getText().toString()+"\n";
+            outToServer.writeBytes(matnr);
+
+            String modifiedSentence = inFromServer.readLine();
+
+            clientSocket.close();
+            TextView output = findViewById(R.id.outputFromServer);
+            output.setText(modifiedSentence);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void doCalculation() {
         EditText mat = findViewById(R.id.matrikelnummer);
