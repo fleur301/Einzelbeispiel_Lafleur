d6551213ea6985d32acb1da511d7cccbdb74c202
Denies Lafleur <delafleur@edu.aau.at>
2021-03-09 10:07:21 +0100

    implemented method for Task2.2

--- app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.einzelbeispiellafleur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void doCalculation() {
+        EditText mat = findViewById(R.id.matrikelnummer);
+        String text = mat.getText().toString();
+        String newText = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (!isPrime(text.charAt(i))) {
+                newText += text.charAt(i);
+            }
+        }
+        newText = arrayToString(sortString(toArray(newText)));
+        TextView calculatedMatNr = findViewById(R.id.calculatedResult);
+        calculatedMatNr.setText(newText);
+    }
+
+    public boolean isPrime (int n) {
+        return n == '2' || n == '3' || n == '5' || n == '7' ? true : false;
+    }
+
+    public char[] toArray(String s) {
+        char[] array = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            array[i] = s.charAt(i);
+        }
+
+        return array;
+    }
+
+    public String arrayToString (char[] c) {
+        String string = "";
+        for (int i = 0; i < c.length; i++) {
+            string += c[i];
+        }
+
+        return string;
+    }
+
+    public char[] sortString(char[] s) {
+        for (int i = 0; i < s.length-1; i++) {
+            for (int j = 0; j < s.length - i - 1; j++) {
+                if (s[j] > s[j + 1]) {
+                    char temp = s[j];
+                    s[j] = s[j + 1];
+                    s[j + 1] = temp;
+                }
+            }
+        }
+        return s;
     }
 }
