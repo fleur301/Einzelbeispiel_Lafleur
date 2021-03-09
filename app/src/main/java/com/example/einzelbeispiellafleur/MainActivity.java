1449032e235d0bb3782a432ea04ab52718c19e58
Denies Lafleur <delafleur@edu.aau.at>
2021-03-09 18:09:45 +0100

    Implementierung von Threads und Änderung in AndroidManifest.xml

--- app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiellafleur/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.einzelbeispiellafleur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,22 +21,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         Button connectToServer = findViewById(R.id.connectionBtn);
         connectToServer.setOnClickListener(
                 //Do Fancy Stuff
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        connectToServer();
+                        TCPConnectionAsClient tcpConnectionAsClient = new TCPConnectionAsClient();
+                        new Thread(tcpConnectionAsClient).start();
                     }
                 }
         );
-
-
-
-
 
         Button calculate = findViewById(R.id.doCalculation);
         calculate.setOnClickListener(
@@ -43,82 +39,92 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                       doCalculation();
+                        SortWithoutPrimes sortWithoutPrimes = new SortWithoutPrimes();
+                        new Thread(sortWithoutPrimes).start();
                     }
                 }
         );
     }
 
-    public void connectToServer() {
-        EditText mat = findViewById(R.id.matrikelnummer);
-        try {
 
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
-
-            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            String matnr = mat.getText().toString()+"\n";
-            outToServer.writeBytes(matnr);
-
-            String modifiedSentence = inFromServer.readLine();
-
-            clientSocket.close();
-            TextView output = findViewById(R.id.outputFromServer);
-            output.setText(modifiedSentence);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void doCalculation() {
-        EditText mat = findViewById(R.id.matrikelnummer);
-        String text = mat.getText().toString();
-        String newText = "";
-        for (int i = 0; i < text.length(); i++) {
-            if (!isPrime(text.charAt(i))) {
-                newText += text.charAt(i);
-            }
-        }
-        newText = arrayToString(sortString(toArray(newText)));
-        TextView calculatedMatNr = findViewById(R.id.calculatedResult);
-        calculatedMatNr.setText(newText);
-    }
-
-    public boolean isPrime (int n) {
-        return n == '2' || n == '3' || n == '5' || n == '7' ? true : false;
-    }
-
-    public char[] toArray(String s) {
-        char[] array = new char[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            array[i] = s.charAt(i);
-        }
-
-        return array;
-    }
-
-    public String arrayToString (char[] c) {
-        String string = "";
-        for (int i = 0; i < c.length; i++) {
-            string += c[i];
-        }
-
-        return string;
-    }
-
-    public char[] sortString(char[] s) {
-        for (int i = 0; i < s.length-1; i++) {
-            for (int j = 0; j < s.length - i - 1; j++) {
-                if (s[j] > s[j + 1]) {
-                    char temp = s[j];
-                    s[j] = s[j + 1];
-                    s[j + 1] = temp;
+    class SortWithoutPrimes implements Runnable {
+        @Override
+        public void run() {
+            Log.i("1", "OnClick");
+            EditText mat = findViewById(R.id.matrikelnummer);
+            String text = mat.getText().toString();
+            String newText = "";
+            for (int i = 0; i < text.length(); i++) {
+                if (!isPrime(text.charAt(i))) {
+                    newText += text.charAt(i);
                 }
             }
+            newText = arrayToString(sortString(toArray(newText)));
+            TextView calculatedMatNr = findViewById(R.id.calculatedResult);
+            calculatedMatNr.setText(newText);
         }
-        return s;
+
+        public boolean isPrime (int n) {
+            return n == '2' || n == '3' || n == '5' || n == '7' ? true : false;
+        }
+
+        public char[] toArray(String s) {
+            char[] array = new char[s.length()];
+            for (int i = 0; i < s.length(); i++) {
+                array[i] = s.charAt(i);
+            }
+
+            return array;
+        }
+
+        public String arrayToString (char[] c) {
+            String string = "";
+            for (int i = 0; i < c.length; i++) {
+                string += c[i];
+            }
+
+            return string;
+        }
+
+        public char[] sortString(char[] s) {
+            for (int i = 0; i < s.length-1; i++) {
+                for (int j = 0; j < s.length - i - 1; j++) {
+                    if (s[j] > s[j + 1]) {
+                        char temp = s[j];
+                        s[j] = s[j + 1];
+                        s[j + 1] = temp;
+                    }
+                }
+            }
+            return s;
+        }
+    }
+
+
+    class TCPConnectionAsClient implements Runnable {
+        @Override
+        public void run() {
+            EditText mat = findViewById(R.id.matrikelnummer);
+            try {
+
+                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+                Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String matnr = mat.getText().toString()+"\n";
+                outToServer.writeBytes(matnr);
+
+                String modifiedSentence = inFromServer.readLine();
+
+                clientSocket.close();
+                TextView output = findViewById(R.id.outputFromServer);
+                output.setText(modifiedSentence);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
